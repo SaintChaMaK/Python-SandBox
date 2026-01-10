5a6a5acdf5f5ce24647c4989d76339605f4f2497
SaintChaMaK <saintchamak@proton.me>
2026-01-10 10:16:18 -0600

    using help() to find Help regarding a function from a command line

---
+++ help.py
@@ -0,0 +1,7 @@
+"""
+# help can Used to print available method regarding a particular built-in function
+"""
+
+#For example
+#print(help(str))
+#print(help(str.upper))
--- sand.py
+++ sand.py
@@ -178,5 +178,7 @@ print(c)"""
 
 ##### ----- 
 
-print(10 > 5)
-print(5 > 10)
+#print(10 > 5)
+#print(5 > 10)
+
+print(help(str))
